b156ce546f7ffb88c80f2be9f2428a3773b3b579
Devmdmainuddin <devmdmainuddin@gmail.com>
2024-11-16 08:39:27 +0600

    error page design

--- client/src/pages/Error/Error.jsx
+++ client/src/pages/Error/Error.jsx
@@ -1,9 +1,52 @@
 import React from 'react';
+import Bredcumb from '../../components/shared/Bredcumb';
+import Container from '../../components/shared/Container';
+import Image from '../../components/shared/Image';
+import { Link } from 'react-router-dom';
+import Navbar from '../../components/shared/Navbar';
+import Footer from '../../components/shared/Footer';
 
 const Error = () => {
     return (
         <div>
-            error
+             <Navbar/>
+            {/* Bredcumb */}
+            <div className="bg-[url('/ab.webp')] bg-cover bg-no-repeat h-[368px] md:h-[456px] lg:h-[654px] flex items-center justify-center ">
+                <Container>
+                    <div className='py-8  md:py-[50px] '>
+
+                        <h2 className='text-white font-inter text-[24px] font-semibold leading-[1.1] tracking-normal text-left capitalize mt-[14px]'>  Error </h2>
+                        <ul className='flex items-center gap-2 mt-3'>
+                            <li className='text-white font-inter text-[14px] font-normal leading-[1.1] tracking-normal text-center capitalize'>
+                                <Link to='/' className="text-sm md:text-xl" >Home</Link>
+
+                                <span className="mx-1 md:mx-2"> - </span>
+                                <Link to='{`/${pathSegments[0]}`}' className="text-sm md:text-xl text-[#FE651B]">
+                                    Error
+                                </Link>
+
+                            </li>
+                        </ul>
+
+                    </div>
+                </Container>
+            </div>
+
+            <Container>
+                <div className='flex justify-center items-center flex-col gap-6 my-[120px]'>
+                    <div className="image">
+                        <Image src="/error.png" alt="Error" />
+                    </div>
+                    <div className="content flex justify-center items-center flex-col">
+
+                        <h2 className='text-[#1a1a1a] text-[25px] font-bold leading-[34px] font-nunito'>Oops! Page not found</h2>
+                        <p className='text-[#6b6161] text-[16px] font-normal leading-[26px] tracking-[0] text-center '>The page you are looking for may have been moved, deleted, or does not exist.</p>
+                        <Link to="/" className=" text-white text-[18px] font-normal leading-[30px]  capitalize py-[15px] px-[33px] bg-gradient-to-r from-[#FE651B] to-[#FFAA52] rounded-full mt-6 inline-block">Go back to home</Link>
+                    </div>
+                </div>
+
+            </Container>
+            <Footer/>
         </div>
     );
 };
--- client/src/pages/Testimonial/Testimonial.jsx
+++ client/src/pages/Testimonial/Testimonial.jsx
@@ -3,19 +3,20 @@ import Container from "../../components/shared/Container";
 import { Swiper, SwiperSlide } from 'swiper/react';
 // import '../../style/popularCourse.css'
 import "swiper/css";
-
+import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import {Autoplay, Pagination } from 'swiper/modules';
+import { Autoplay, Pagination, Navigation, } from 'swiper/modules';
 
 import { Rating, Star } from "@smastrom/react-rating";
+import Image from "../../components/shared/Image";
 
 const Testimonial = () => {
     const myStyles = {
         itemShapes: Star,
         activeFillColor: '#ffb700',
         inactiveFillColor: '#fbf1a9'
-        
-      }
+
+    }
     const Testimonial = [
         {
             id: 1,
@@ -60,50 +61,112 @@ const Testimonial = () => {
     ]
     return (
         <div>
-            <Bredcumb/>
+            <Bredcumb />
             <div>
 
-            <Container>
-            <div className="my-[120px]">
-                <Swiper
+                <Container>
+                    <div className="my-[120px]">
+                    <div className="flex justify-center items-center flex-col">
+                            <div className="flex gap-2 items-center">
+                                <img src="/icon1.png" alt="" />
+                                <h3 className="text-[#3661fc] font-dm-sans text-[20px] font-medium leading-[26px]">Testimonial</h3>
+                            </div>
+                            <h2 className="text-[#1a1a1a] mt-4 font-nunito text-[46px] font-extrabold leading-[56px]">What Our Client Say</h2>
+                        </div>
+                        <Swiper
                             loop={true}
-                          
                             pagination={true}
                             autoplay={{
                                 delay: 2500,
                                 disableOnInteraction: false,
-                              }}
-                            modules={[Pagination,Autoplay]}
-                             breakpoints={{
-                           
+                            }}
+                            modules={[Pagination, Autoplay]}
+                            breakpoints={{
+
                                 1026: {
                                     slidesPerView: 2,
                                     slidesPerGroup: 1,
                                 },
-                               
+
                             }}
                             spaceBetween={24}
 
-                            className="mySwiper testimonial"
+                            className="mySwiper testimonial mt-[46px]"
                         >
-                          {Testimonial.map((item, idx) => <SwiperSlide key={idx}>
-                                  <div >
-                                        <div className="flex justify-between items-center">
-                                            <img src="/qua.png" alt="testimonial" />
-                                            <div className="bg-[#F4F4FE] py-[15px] px-5 rounded-full"><Rating  style={{ maxWidth: 250 }} value={item.rating} itemStyles={myStyles} /></div>
-                                        </div>
-                                        <p className="text-[#1a1a1a] font-[DM Sans] text-[20px] font-normal leading-[30px] max-w-[561px] mt-[30px]">{item.description}</p>
-                                        <h2 className="font-bold text-[25px] leading-[1px] text-[#1A1A1A mt-[30px]">{item.name}</h2>
-                                        <h3 className="font-normal text-[16px] leading-[26px] text-[#6B6161] mt-3">{item.role}</h3>
+                            {Testimonial.map((item, idx) => <SwiperSlide key={idx}>
+                                <div >
+                                    <div className="flex justify-between items-center">
+                                        <img src="/qua.png" alt="testimonial" />
+                                        <div className="bg-[#F4F4FE] py-[15px] px-5 rounded-full"><Rating style={{ maxWidth: 200 }} value={item.rating} itemStyles={myStyles} /></div>
                                     </div>
-                                </SwiperSlide>)}
-                          
+                                    <p className="text-[#1a1a1a] font-[DM Sans] text-[20px] font-normal leading-[30px] max-w-[561px] mt-[30px]">{item.description}</p>
+                                    <div className="flex items-center gap-3 mt-[30px]">
+                                        <div className="w-[62px] h-[62px] rounded-full">
+                                            <Image src={item.image} alt="testimonial" className='rounded-full' />
+                                        </div>
+                                        <div className="">
+                                            <h2 className="font-bold text-[25px] leading-[1px] text-[#1A1A1A]">{item.name}</h2>
+                                            <h3 className="font-normal text-[16px] leading-[26px] text-[#6B6161] mt-3">{item.role}</h3>
+                                        </div>
+                                    </div>
+                                </div>
+                            </SwiperSlide>)}
+
 
 
 
                         </Swiper>
-                </div>
-            </Container>
+                    </div>
+                    <div className="my-[120px]  mx-auto">
+                        <div className="flex justify-center items-center flex-col">
+                            <div className="flex gap-2 items-center">
+                                <img src="/icon1.png" alt="" />
+                                <h3 className="text-[#3661fc] font-dm-sans text-[20px] font-medium leading-[26px]">Testimonial</h3>
+                            </div>
+                            <h2 className="text-[#1a1a1a] mt-4 font-nunito text-[46px] font-extrabold leading-[56px]">What Our Client Say</h2>
+                        </div>
+                        <Swiper
+                            loop={true}
+                            navigation={true}
+                            autoplay={{
+                                delay: 2500,
+                                disableOnInteraction: false,
+                            }}
+                            modules={[Navigation, Autoplay]}
+
+                            spaceBetween={0}
+
+                            className="mySwiper testimonial2 mt-[46px]"
+                        >
+                            {Testimonial.map((item, idx) => <SwiperSlide key={idx} className="">
+                                <div >
+                                    <div className="flex justify-between items-center max-w-[886px] mx-auto">
+                                        <img src="/qua.png" alt="testimonial" />
+                                        <div className="flex justify-between items-center gap-3">
+                                            <div className="w-[50px] h-[50px] md:w-[80px] md:h-[80px] rounded-full">
+                                                <Image src={item.image} alt="testimonial" className='rounded-full' />
+                                            </div>
+                                            <div>
+                                                <h2 className="font-bold text-[25px] leading-[1px] text-[#1A1A1A ">{item.name}</h2>
+                                                <h3 className="font-normal text-[16px] leading-[26px] text-[#6B6161] mt-3">{item.role}</h3>
+                                            </div>
+                                        </div>
+                                        <div className="bg-[#F4F4FE] p-2 md:py-[15px] md:px-5 rounded-full"><Rating style={{ maxWidth: 150 }} value={item.rating} itemStyles={myStyles} /></div>
+                                    </div>
+                                    <div className="flex flex-col justify-center items-center max-w-[886px] mx-auto">
+                                        <p className="text-[#1a1a1a] font-[DM Sans] text-center text-[20px] font-normal leading-[30px]  mt-[30px]">{item.description}</p>
+                                    </div>
+
+
+                                </div>
+                            </SwiperSlide>)}
+
+
+
+
+                        </Swiper>
+                    </div>
+                </Container>
             </div>
         </div>
     );
